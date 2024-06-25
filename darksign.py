39e7527bd49b0b3e64e96b49f3ee9b9be37c0a75
K.R. Zentner <krzentner@gmail.com>
2024-06-25 02:37:27 -0700

    Tried to make Pdb async

--- darksign.py
+++ darksign.py
@@ -6,6 +6,8 @@ import time
 import functools
 import threading
 import pdb
+import termios
+import asyncio
 
 old_except_hook = None
 
@@ -16,27 +18,78 @@ _LOGGER.setLevel(logging.DEBUG)
 _to_watcher = queue.Queue()
 
 
+async def connect_stdin_stdout():
+    loop = asyncio.get_event_loop()
+    reader = asyncio.StreamReader()
+    protocol = asyncio.StreamReaderProtocol(reader)
+    await loop.connect_read_pipe(lambda: protocol, sys.stdin)
+    w_transport, w_protocol = await loop.connect_write_pipe(asyncio.streams.FlowControlMixin, sys.stdout)
+    writer = asyncio.StreamWriter(w_transport, w_protocol, reader, loop)
+    return reader, writer
+
+
 class DarksignPdb(pdb.Pdb):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.async_task_loop = None
 
     def _cmdloop(self) -> None:
         self.allow_kbdint = True
         self.cmdloop()
+        # try:
+        #     self.async_task_loop = self.async_cmdloop()
+        #     asyncio.run(self.async_task_loop)
+        # finally:
+        #     termios.tcflush(sys.stdin, termios.TCIFLUSH)
         self.allow_kbdint = False
 
-
-def _get_debugger_from_tb(tb):
-    printed_message = False
-    while True:
-        time.sleep(0.1)
-        if tb.tb_frame.f_trace is None:
-            _LOGGER.debug(f"Waiting for debugger to attach to {tb.tb_frame!r}")
-            printed_message = True
-        else:
-            if printed_message:
-                _LOGGER.debug(f"Debugger attached to {tb.tb_frame!r}")
-            break
-    debugger = tb.tb_frame.f_trace.__self__
-    return debugger
+    async def async_cmdloop(self, intro=None):
+        reader, writer = await connect_stdin_stdout()
+        self.preloop()
+        if self.use_rawinput and self.completekey:
+            try:
+                print('setting up readline')
+                import readline
+                self.old_completer = readline.get_completer()
+                readline.set_completer(self.complete)
+                readline.parse_and_bind(self.completekey+": complete")
+                print('set up readline')
+            except ImportError:
+                pass
+        try:
+            if intro is not None:
+                self.intro = intro
+            if self.intro:
+                writer.write((str(self.intro)+"\n").encode())
+            stop = None
+            while not stop:
+                if self.cmdqueue:
+                    line = self.cmdqueue.pop(0)
+                else:
+                    writer.write(self.prompt.encode())
+                    await writer.drain()
+                    line = ''
+                    in_bytes = b''
+                    while '\n' not in line:
+                        in_bytes = await reader.readline()
+                        line += in_bytes.decode()
+                        print('in_bytes', in_bytes)
+                    if not len(in_bytes):
+                        line = 'EOF'
+                    else:
+                        line = line.rstrip('\r\n')
+                line = self.precmd(line)
+                stop = self.onecmd(line)
+                stop = self.postcmd(stop, line)
+            self.postloop()
+        finally:
+            if self.use_rawinput and self.completekey:
+                try:
+                    import readline
+                    readline.set_completer(self.old_completer)
+                except ImportError:
+                    pass
 
 
 PROGRAM_SHOULD_EXIT = False
@@ -52,30 +105,14 @@ def watcher_main():
         cmd, contents = _to_watcher.get(block=True)
         if cmd == 'tb':
             thread_id, traceback, debugger = contents
+            # time.sleep(2.0)
+            # print("Code was patched, interrupting debugger...")
+            # ctype_async_raise(thread_id, CodePatched())
             # debugger = _get_debugger_from_tb(traceback)
-            print('debugger', debugger)
-        elif cmd == 'shutdown':
-            thread_id = contents
-            ctype_async_raise(thread_id, IntentionalExit())
+            # print('debugger', debugger)
 
-
-class IntentionalExit(KeyboardInterrupt):
+class CodePatched(Exception):
     pass
-
-
-def ctype_async_raise(target_tid, exception):
-    ret = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(target_tid),
-                                                     ctypes.py_object(exception))
-    # ref: http://docs.python.org/c-api/init.html#PyThreadState_SetAsyncExc
-    if ret == 0:
-        raise ValueError("Invalid thread ID")
-    elif ret > 1:
-        # Huh? Why would we notify more than one threads?
-        # Because we punch a hole into C level interpreter.
-        # So it is better to clean up the mess.
-        ctypes.pythonapi.PyThreadState_SetAsyncExc(target_tid, NULL)
-        raise SystemError("PyThreadState_SetAsyncExc failed")
-
 
 
 WATCHER_THREAD = threading.Thread(target=watcher_main, daemon=True)
@@ -103,8 +140,14 @@ def wrap(func):
                                      (threading.get_ident(),
                                       traceback,
                                       debugger)))
-                    debugger.reset()
-                    debugger.interaction(None, traceback)
+                    if traceback is not None:
+                        debugger.reset()
+                        debugger.setup(traceback.tb_frame, traceback)
+                        try:
+                            debugger._cmdloop()
+                        except KeyboardInterrupt:
+                            PROGRAM_SHOULD_EXIT = True
+                            raise e
                     # breakpoint()
                 # PROGRAM_SHOULD_EXIT could have been set in the debugger
                 if PROGRAM_SHOULD_EXIT:
