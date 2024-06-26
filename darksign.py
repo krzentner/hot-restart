22722e01b8819b138c39a985482a3fa91620555f
K.R. Zentner <krzentner@gmail.com>
2024-06-25 23:25:43 -0700

    Implemented ast path based source loading

--- darksign.py
+++ darksign.py
@@ -6,9 +6,11 @@ import threading
 import pdb
 import inspect
 import tokenize
-# from importlib import util
-# import importlib.machinery
 import tempfile
+import ast
+from typing import Any
+import textwrap
+import types
 
 old_except_hook = None
 
@@ -21,6 +23,10 @@ _to_watcher = queue.Queue()
 
 class DarksignPdb(pdb.Pdb):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.program_should_exit = False
+
     def _cmdloop(self) -> None:
         # The only difference vs normal Pdb is that this function does not
         # catch KeyboardInterrupt. Without this, exiting a darksign program can
@@ -29,6 +35,11 @@ class DarksignPdb(pdb.Pdb):
         self.allow_kbdint = True
         self.cmdloop()
         self.allow_kbdint = False
+
+    def set_quit(self):
+        super().set_quit()
+        self.program_should_exit = True
+
 
 
 PROGRAM_SHOULD_EXIT = False
@@ -41,26 +52,135 @@ def exit():
 TMP_SOURCE_FILES = {}
 
 
-def reload_function(func):
+class ReloadException(ValueError):
+    pass
+
+
+class FindDefPath(ast.NodeVisitor):
+    def __init__(self, target_name: str, target_lineno: int):
+        super().__init__()
+        self.target_name = target_name
+        self.target_lineno = target_lineno
+        self.found_def_paths = []
+        self.path_now = []
+
+    def generic_visit(self, node: ast.AST) -> Any:
+        if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)):
+            self.path_now.append(node)
+            if node.lineno == self.target_lineno and node.name == self.target_name:
+                self.found_def_paths.append([node.name for node in self.path_now])
+            res = super().generic_visit(node)
+            self.path_now.pop()
+            return res
+        else:
+            return super().generic_visit(node)
+
+
+class GetDefFromePath(ast.NodeVisitor):
+    def __init__(self, target_path: list[str]):
+        super().__init__()
+        self.target_path = target_path
+        self.depth = 0
+        self.found_defs = []
+
+    def generic_visit(self, node: ast.AST) -> Any:
+        if isinstance(node, (ast.FunctionDef, ast.AsyncFunctionDef, ast.ClassDef)):
+            if node.name == self.target_path[self.depth]:
+                self.depth += 1
+                if self.depth == len(self.target_path):
+                    self.found_defs.append(node)
+                res = super().generic_visit(node)
+                self.depth -= 1
+                return res
+        return super().generic_visit(node)
+
+
+def get_sourcelines(module_source, module_ast, def_path):
+    visitor = GetDefFromePath(target_path=def_path)
+    visitor.visit(module_ast)
     try:
-        module = inspect.getmodule(func)
+        definition = visitor.found_defs[0]
+    except IndexError:
+        raise ReloadException(f"Could not find defintion for {'.'.join(def_path)}")
+    src_segment = ast.get_source_segment(module_source, definition, padded=True)
+    if src_segment is None:
+        raise ReloadException(f"Definition of {'.'.join(def_path)} does not have a source segment")
+    src_segment = textwrap.dedent(src_segment)
+    return src_segment, definition.lineno
+
+
+ORIGINAL_SOURCE_CACHE = {}
+ORIGINAL_SOURCE_AST_CACHE = {}
+
+
+def get_def_path(func):
+    source_filename = inspect.getsourcefile(func)
+    if source_filename not in ORIGINAL_SOURCE_AST_CACHE:
+        with open(source_filename, 'r') as f:
+            content = f.read()
+            ORIGINAL_SOURCE_CACHE[source_filename] = content
+            ORIGINAL_SOURCE_AST_CACHE[source_filename] = ast.parse(content)
+    module_ast = ORIGINAL_SOURCE_AST_CACHE[source_filename]
+    func_name = func.__name__
+    original_lines, func_lineno = inspect.getsourcelines(func)
+    del original_lines
+    visitor = FindDefPath(target_name=func_name, target_lineno=func_lineno + 1)
+    visitor.visit(module_ast)
+    if len(visitor.found_def_paths) == 0:
+        _LOGGER.error(f"Could not find definition of {func!r}")
+        return None
+    def_path = visitor.found_def_paths[0]
+    src_segment, lineno = get_sourcelines(
+            ORIGINAL_SOURCE_CACHE[source_filename], module_ast, def_path)
+    del lineno
+    return visitor.found_def_paths[0]
+
+
+def reload_function(def_path, func):
+    # print(dir(func))
+    # print(func.__closure__)
+    try:
         source_filename = inspect.getsourcefile(func)
+        with open(source_filename, 'r') as f:
+            all_source = f.read()
+        src_ast = ast.parse(all_source, filename=source_filename)
+
+        module = inspect.getmodule(func)
         if source_filename is None:
             # Probably used in an interactive session or something, which
             # doesn't work :/
             _LOGGER.error(f"Could not reload {func!r}: No known source file")
             return None
-        source_lines, start_linenum = inspect.getsourcelines(func)
+        # source_lines, start_linenum = inspect.getsourcelines(func)
+
+        source_lines, start_linenum = get_sourcelines(
+            all_source, src_ast, def_path)
+
+        matched_lineno_src = ('\n' * (start_linenum - 1)) + source_lines
+
         temp_source = tempfile.TemporaryFile(suffix='.py', mode='w')
-        contents = (['\n'] * (start_linenum - 1)) + source_lines
-        temp_source.writelines(contents)
-        file_content = ''.join(contents)
-        code = compile(file_content, source_filename, 'exec')
-        loc = {}
+        temp_source.write(matched_lineno_src)
+        print('=== Reloading ===')
+        print(matched_lineno_src)
+        code = compile(matched_lineno_src, source_filename, 'exec')
+        context = module
+        for var_name in def_path[:-1]:
+            context = getattr(context, var_name)
+        loc = dict(vars(context))
+        print('Executing with context:', context)
         exec(code, vars(module), loc)
-        new_func = loc.get(func.__name__, None)
+        raw_func = loc.get(func.__name__, None)
+        # new_func = types.FunctionType(
+        #     raw_func.__code__,
+        #     func.__globals__,
+        #     func.__name__,
+        #     raw_func.__defaults__,
+        #     func.__closure__, # TODO: Match up cell names, instead of blindly copying __closure__
+        # )
+        print('=== Done Reloading ===')
         if new_func is not None:
             TMP_SOURCE_FILES[source_filename] = temp_source
+        print(f"Reloaded {func.__name__!r}")
         return new_func
     except (OSError, SyntaxError, tokenize.TokenError) as e:
         _LOGGER.error(f"Could not reload {func!r}: {e}")
@@ -77,10 +197,15 @@ def watcher_main():
             # ctype_async_raise(thread_id, CodePatched())
             # debugger = _get_debugger_from_tb(traceback)
             # print('debugger', debugger)
+        elif cmd == 'watch':
+            source_filename, original_obj = contents
+        else:
+            _LOGGER.error(f"Uknown watcher command: {cmd}")
 
 
 SHOULD_HOT_RELOAD = True
 WATCHER_THREAD = None
+PRINT_HELP_MESSAGE = False
 
 
 def wrap(original_func):
@@ -88,13 +213,18 @@ def wrap(original_func):
     if WATCHER_THREAD is None and SHOULD_HOT_RELOAD:
         WATCHER_THREAD = threading.Thread(target=watcher_main, daemon=True)
         WATCHER_THREAD.start()
+    def_path = get_def_path(original_func)
+    source_filename = inspect.getsourcefile(original_func)
+    # _to_watcher.put(('watch', (source_filename, original_func)))
     func_now = original_func
     @functools.wraps(original_func)
     def darksign_wrapper(*args, **kwargs):
         nonlocal func_now
         global PROGRAM_SHOULD_EXIT
+        global PRINT_HELP_MESSAGE
+        should_exit_this_level = False
         restart_count = 0
-        while not PROGRAM_SHOULD_EXIT:
+        while not PROGRAM_SHOULD_EXIT and not should_exit_this_level:
             if restart_count > 0:
                 _LOGGER.debug(f"Restarting {func_now!r}")
             try:
@@ -105,10 +235,39 @@ def wrap(original_func):
                     # The user is probably intentionally exiting
                     PROGRAM_SHOULD_EXIT = True
 
-                if not PROGRAM_SHOULD_EXIT:
+                if not PROGRAM_SHOULD_EXIT and not should_exit_this_level:
                     traceback = sys.exc_info()[2]
+
+                    # Print basic commands
+                    print()
+                    # e_msg = str(e)
+                    e_msg = repr(e)
+                    if not e_msg:
+                        e_msg = repr(e)
+                    print(f"{func_now.__name__}: {e_msg}")
+                    if PRINT_HELP_MESSAGE:
+                        print(f"(c)ontinue to revive {func_now.__name__!r}")
+                        print("Ctrl-C to re-raise exception")
+                        print("(q)uit to exit program")
+                        PRINT_HELP_MESSAGE = False
+                    print()
                     debugger = DarksignPdb()
                     debugger.reset()
+
+                    # Adjust starting frame of debugger
+                    height = 0
+                    tb_next = traceback.tb_next
+                    while tb_next.tb_next is not None:
+                        height += 1
+                        tb_next = tb_next.tb_next
+
+                    for _ in range(height):
+                        debugger.cmdqueue.append('u')
+
+                    # Show function source
+                    # TODO(krzentner): Use original source, instead of
+                    # re-fetching from file (which may be out of date)
+                    debugger.cmdqueue.append('ll')
                     _to_watcher.put(('tb',
                                      (threading.get_ident(),
                                       traceback,
@@ -117,13 +276,16 @@ def wrap(original_func):
                         debugger.interaction(None, traceback)
                     except KeyboardInterrupt:
                         # If user input KeyboardInterrupt from the debugger,
-                        # exit the program.
+                        # break up one level.
+                        should_exit_this_level = True
+                    if debugger.program_should_exit:
                         PROGRAM_SHOULD_EXIT = True
-                    new_func = reload_function(original_func)
+
+                if PROGRAM_SHOULD_EXIT or should_exit_this_level:
+                    raise e
+                else:
+                    new_func = reload_function(def_path, original_func)
                     if new_func is not None:
                         func_now = new_func
-
-                if PROGRAM_SHOULD_EXIT:
-                    raise e
             restart_count += 1
     return darksign_wrapper
--- test.py
+++ test.py
@@ -6,21 +6,33 @@ def outer():
     print('in outer')
     x = 1
     y = 1 + x
+    inner_inst = Inner()
     try:
-        z, q = inner(y, k=[])
+        z, q = inner_inst.inner(y, k=[])
     except AssertionError as e:
         raise e
     return y + z
 
 print('hi')
 
-@darksign.wrap
-def inner(y_inner, k='test'):
-    print('in inner')
-    z = y_inner ** 2
-    assert False
-    k.append(z)
-    return z ** 2, k
+class Parent:
 
+    def inner(self):
+        print('in parent.inner')
+
+class Inner(Parent):
+
+    @darksign.wrap
+    def inner(self, y_inner, k='test'):
+        super().inner()
+        print(self)
+        print('in inner')
+        z = y_inner ** 2
+        mini()
+        k.append(z)
+        return z ** 2, k
+
+def mini():
+    assert False
 
 outer()
